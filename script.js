b784fe5b1a3e4b1dc3f16a9ce1f1fde0252d0e33
Tanish Shah <stanish994@gmail.com>
2021-01-16 15:59:41 +0530

    Final Commit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var count = 0;
 function GameStart(){
 
     document.getElementById("MainGame").style.display = "block";
-    document.getElementById("SubPart").innerHTML = "Enter Your Number :-"
+    document.getElementById("SubPart").innerHTML = "Enter Your Number"
         
 }
 
@@ -14,13 +14,19 @@ function computer(){
     if(count < 10){
     count = count + 1;
     a = document.getElementById("fname").value;
+    if(a == ""){
+        c = "Empty Space Entered"
+    }
+    else{
     b = (parseInt(a) + ((Math.floor(Math.random()*100))+1));
     c = "The Computer's Value is :- " + b;
     document.getElementById("CompPart").innerHTML = c ;
-    document.getElementById("count").innerHTML = count + " / 10" ;
+    document.getElementById("count").innerHTML = "Chances:- " + count + " / 10" ;
+    document.getElementById("fname").value = "";
     }
+}
     else{
-      var j = prompt("Do you want to continue?", "Yes");
+      var j = prompt("Do you want to continue? Type Yes or NO " , "Yes");
       if(j=="yes" || j=="y" || j=="YES" || j == "Yes"){
           count = 0;
           a = 0;
@@ -30,6 +36,7 @@ function computer(){
         else{
            document.getElementById("MainGame").style.display = "none";
            document.getElementById("GameEnd").style.display = "block";
+           document.getElementById("GameEnd").style.transitionDuration = "4s";
            document.getElementById("button").style.display = "none";
         }
     }
